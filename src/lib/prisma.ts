74e3b734f30fd8e98240d393c48ec3a19495c934
Бахтияр Мингазов <bahtiarmingazov@MacBook-Pro-Bahtiar.local>
2025-09-25 11:54:37 +0300

    Настройка соединения с Prisma Cloud и добавление постдеплойных миграций

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from '../generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+function createPrismaClient() {
+  return new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  }).$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
